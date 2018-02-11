8795e2664bf8bf84e9c6cd97e5f1fde3996fe937
Marissa Reinke <reinke.marissa@gmail.com>
2018-02-10 18:44:57 -0700

    Logic for dummy login and error

--- login/login.js
+++ login/login.js
@@ -1,1 +1,26 @@
-console.log('made it')
+document.getElementById('form-button').addEventListener('click', function(e) {
+  e.preventDefault();
+  var email = document.getElementById('input-email').value === 'pam@isthebest.com';
+  var password = document.getElementById('input-password').value === 'puppiesandshit';
+  authorizeLogin(email, password);
+});
+
+function authorizeLogin(email, password) {
+  if (email && password) {
+    window.location = './success.html'
+  } else {
+    document.getElementById("login-error").innerHTML = "Oops! Typo? Try again.";
+  }
+}
+
+
+//
+//         if (ValidEmail === true && ValidPassword === true) {
+//             $('.valid').css('display', 'block');
+//             window.location = "http://arkev.com"; // go to home.html
+//         }
+//         else {
+//             $('.error').css('display', 'block'); // show error msg
+//         }
+//     });
+// });
