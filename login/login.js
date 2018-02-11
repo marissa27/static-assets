995e17137fb2c72f864cdabf930f41fe275a0dd0
Marissa Reinke <reinke.marissa@gmail.com>
2018-02-10 19:05:57 -0700

    Begin remember email logic

--- login/login.js
+++ login/login.js
@@ -1,3 +1,12 @@
+document.onreadystatechange = function () {
+  if (document.readyState == "interactive") {
+    var email = localStorage.getItem('rememberEmail')
+    if (email) {
+      document.getElementById("input-email").innerHTML = email;
+    }
+  }
+}
+
 document.getElementById('form-button').addEventListener('click', function(e) {
   e.preventDefault();
   var email = document.getElementById('input-email').value === 'pam@isthebest.com';
@@ -18,3 +27,8 @@ function clearInputs() {
   document.getElementById("input-email").value = "";
   document.getElementById("input-password").value = "";
 }
+
+document.getElementById('remember-me').addEventListener('click', function() {
+  var email = document.getElementById('input-email');
+  localStorage.setItem('rememberEmail', email);
+});
